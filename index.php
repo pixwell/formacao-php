79deac8a41a26c09747cd8432b7ad7c76ecd359e
Marcia Silva <contato.pix@gmail.com>
2025-09-30 14:15:03 -0300

    HTML inicial

--- index.php
+++ index.php
@@ -3,10 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Hello!!</title>
 </head>
 <body>
     <h1>Hello World!</h1>
+    <h2>Esse é um título H2</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam autem neque deserunt officiis, laboriosam ex recusandae consequatur. Libero dignissimos obcaecati facilis natus repudiandae, numquam eum incidunt odit pariatur, sint quasi.</p>
     <?php
         phpinfo();
     ?>
