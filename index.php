3f7add7e516413306918cb40695990222b77161e
Marcia Silva <contato.pix@gmail.com>
2025-10-01 17:18:59 -0300

    Refatoração do projeto

--- index.php
+++ index.php
@@ -3,13 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="dist/css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Hello!!</title>
 </head>
 <body>
     <h1 class="bg-red-500 p-4">Hello World!</h1>
     <h2>Esse é um título H2</h2>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam autem neque deserunt officiis, laboriosam ex recusandae consequatur. Libero dignissimos obcaecati facilis natus repudiandae, numquam eum incidunt odit pariatur, sint quasi.</p>
+    <!-- Classes de teste -->
+    <div class="bg-red-500 text-white p-4">Teste vermelho</div>
+    <div class="bg-blue-500 text-white p-4">Teste azul</div>
+    <button class="btn-primary">Classe customizada</button>
     <?php phpinfo(); ?>
 </body>
 </html>
