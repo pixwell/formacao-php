63f99fa42141e87d1bf0a7db5b72d55c1e6a87bb
Marcia Silva <contato.pix@gmail.com>
2025-10-01 17:21:59 -0300

    Inclusão de HTML com classes CSS para teste

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <h1 class="bg-red-500 p-4">Hello World!</h1>
     <h2>Esse é um título H2</h2>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam autem neque deserunt officiis, laboriosam ex recusandae consequatur. Libero dignissimos obcaecati facilis natus repudiandae, numquam eum incidunt odit pariatur, sint quasi.</p>
-    <!-- Classes de teste -->
+    
     <div class="bg-red-500 text-white p-4">Teste vermelho</div>
     <div class="bg-blue-500 text-white p-4">Teste azul</div>
     <button class="btn-primary">Classe customizada</button>
